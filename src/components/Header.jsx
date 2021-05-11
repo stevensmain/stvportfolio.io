f3c0e7cc79001f8330703724bc8912cd04f49464
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2021-05-11 08:18:01 -0400

    header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react'
 import { Nav } from './Nav'
 import Aos from 'aos';
 import 'aos/dist/aos.css';
+import headerimg  from '../images/head.png';
 
 export const Header = () => {
     useEffect(() => {
@@ -10,10 +11,15 @@ export const Header = () => {
     return (
         <div id= "main">
             <Nav />
-            <div className= "name">
-                <h1 data-aos="fade-left" >Hi, my name is <span>Stivens Carrasquel</span></h1>
-                <p data-aos="fade-left" className = "details">I´m a Senior Technical in Computer Science. Currenttly, I´m a Frontend Javascript Developer focused on build accesible human-centered products.</p>
-                <a href="#about" className= "cv-btn">Read More</a>
+            <div className="header__content">
+                <div className= "name">
+                    <h1 data-aos="fade-left" >Hi, my name is <span>Stivens Carrasquel</span></h1>
+                    <p data-aos="fade-left" className = "details">I´m a Senior Technical in Computer Science. Currenttly, I´m a Frontend Javascript Developer focused on build accesible human-centered products.</p>
+                    <a href="#about" className= "cv-btn">Read More</a>
+                </div>
+                <div className="header-image">
+                        <img src= { headerimg } alt="about__image" />
+                </div>
             </div>
         </div>
     )
