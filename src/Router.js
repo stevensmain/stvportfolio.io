bcbab77b3c34cb7ff3e1012c91bf4a0373ee3572
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2021-05-21 16:22:22 -0400

    Test

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import  App   from '../App';
-import { Pdf }  from '../components/Pdf';
+import  App   from './App';
+import { Pdf }  from './components/Pdf';
 
 export const Router = () => {
     return (
